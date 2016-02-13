4802de394a9fdf8927b285f6d693b2eb57b6202a
Jake Vivian <jacob@sadsoft.com>
2016-02-13 00:19:48 +0000

    Changes to make business.py more secure (protected properties in classes, etc)

--- business.py
+++ business.py
@@ -1,10 +1,10 @@
 class Person:
-    name     = None
-    position = None
+    _name     = None
+    _position = None
 
-    def __init__(self, name, position):
-        self.name = name
-        self.position = position
+    def __init__(self, _name, _position):
+        self.name = _name
+        self.position = _position
 
 class Business:
     def __init__(self, name, staff):
@@ -12,29 +12,33 @@ class Business:
         self.staff = staff
     def hire(self, person):
         self.staff.add(person)
+    def getStaffMembers(self):
+        return self.staff.list()
     def getCompanyName(self):
         return self.name
 
 class Staff:
-    members = []
-    def __init__(self):
-        pass
+    _members = None
+    def __init__(self, members):
+        self._members = members;
     def add(self, person):
-        self.members.append(person)
+        self._members.append(person)
+    def list(self):
+        return self._members
 
 # An example how to call this and build the objects out:
 owner = Person('Sean','Owner')
 secondowner = Person('Jackie','Leecher')
-staff = Staff()
+staff = Staff([owner,secondowner]);
 scs = Business('SCS', staff)
-scs.hire(owner)
-scs.hire(secondowner)
+owner._name = 'Digi' #not possible now :)
 jake = Person('Jake', 'Tech')
 colby = Person('Colby', 'Tech')
 scs.hire(jake)
 scs.hire(colby)
+
 print "Company report for " + scs.getCompanyName()
 print "====="
-for member in staff.members:
+for member in scs.getStaffMembers():
     print member.name + ", with position: " + member.position;
 print "Yeee, we're at the end..."
